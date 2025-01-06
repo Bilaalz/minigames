e9c59c0f13731ff308b2d2ea1e89390afa3a89f3
Bilaal Khan <k.bilaal123@gmail.com>
2025-01-05 19:41:55 -0500

    myfirstgameee

---
+++ guessinggamechallenge/guessnumbergame.py
@@ -0,0 +1,51 @@
+import random 
+
+num = random.randint(0,101) # Picks a random integer from 1 to 100
+
+#Printing an introdcution to the game
+print("Welcome to the GUESSING NUMBER GAME!!")
+print("I am thinking of a number from 0 to 100 :)")
+print("If your initial guess is 10 within my number I'll tell you you're WARM ")
+print("If your initial guess is 10 further my number I'll tell you you're COLD :( ")
+print("If your guess is farther than your most recent guess, I'll say you're getting COLDER")
+print("If your guess is closer than your most recent guess, I'll say you're getting WARMER")
+print("LET'S PLAY!")
+
+guesses = [0] # placeholder to track guesses
+
+while True:
+
+    # we can copy the code from above to take an input
+    guess = int(input("I'm thinking of a number between 1 and 100.\n  What is your guess? "))
+    
+    if guess < 1 or guess > 100:
+        print('OUT OF BOUNDS! Please try again: ')
+        continue
+    
+    # here we compare the player's guess to our number
+    if guess == num:
+        print(f'CONGRATULATIONS, YOU GUESSED IT IN ONLY {len(guesses)} GUESSES!!')
+        break
+        
+    # if guess is incorrect, add guess to the list
+    guesses.append(guess)
+    
+    # when testing the first guess, guesses[-2]==0, which evaluates to False
+    # and brings us down to the second section
+    
+    if guesses[-2]:  
+        if abs(num-guess) < abs(num-guesses[-2]):
+            print('WARMER!')
+        else:
+            print('COLDER!')
+   
+    else:
+        if abs(num-guess) <= 10:
+            print('WARM!')
+        else:
+            print('COLD!')
+
+            
+
+
+
